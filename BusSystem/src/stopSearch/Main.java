d30f264de992581a78a621d071ac34da70123b9f
aaronayr <aaryan@tcd.ie>
2022-04-08 15:23:21 +0100

    Remade q2 main class

---
+++ BusSystem/src/stopSearch/Main.java
@@ -0,0 +1,24 @@
+package stopSearch;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String x = "FLAGSTOP laaa street";
+		swap(x);
+	}
+	
+	public static void swap(String busStop)
+	{
+		int space = busStop.indexOf(" ");
+		
+		System.out.println(busStop);
+		if (busStop.indexOf("B ") == 1 || busStop.indexOf("FLAGSTOP") == 0) 
+		{
+			String prefibusStop = busStop.substring(0, space);
+			String other = busStop.substring(space+1, busStop.length());
+			busStop = other + " " + prefibusStop;
+		}
+		System.out.println(busStop);
+	}
+
+}
--- BusSystem/src/stopSearch/MovePrefix.java
+++
@@ -1,9 +0,0 @@
-package stopSearch;
-
-public class MovePrefix {
-
-	public static void main(String[] args) {
-		System.out.println(10);
-	}
-
-}
